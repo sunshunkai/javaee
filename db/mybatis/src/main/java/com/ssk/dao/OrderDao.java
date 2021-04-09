171df5fb59555d38d61347bcce0a2bc3df3dfda9
孙顺凯 <shunkai.ssk@alibaba-inc.com>
2021-04-09 15:11:31 +0800

    tk.mybatis

---
+++ db/mybatis/src/main/java/com/ssk/dao/OrderDao.java
@@ -0,0 +1,8 @@
+package com.ssk.dao;
+
+/**
+ * @author 孙顺凯（惊云）
+ * @date 2021/4/9
+ */
+public interface OrderDao {
+}
---
+++ db/mybatis/src/main/java/com/ssk/mode/OrderDO.java
@@ -0,0 +1,8 @@
+package com.ssk.mode;
+
+/**
+ * @author 孙顺凯（惊云）
+ * @date 2021/4/9
+ */
+public class OrderDO {
+}
